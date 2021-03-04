58717fdb67fb09c29394b8232973560a7c12214e
amitnyc83 <amit_nyc@live.com>
2021-03-04 09:27:48 -0500

    conditional rendering of best makeup guarantee & ingredients

--- src/Accordion.js
+++ src/Accordion.js
@@ -1,29 +1,94 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 
-export default function Accordion({ productIngredients}) {
-  const [isOpen, setIsOpen] = useState(false);
-  console.log(productIngredients)
-    return (
-        <div className="product-ingredients">
-           <div 
-             className="product-ingredients__title"
-             onClick={() => setIsOpen(!isOpen)}
-            >
-             <h4>Ingredients
-             <div className="product-ingredients__indicator">
-                 {isOpen ? '-' : '+'}
-             </div>
-             </h4>
+
+export default function Accordion({ productIngredients, productGuarantee}) {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const [guarantee, setGuarantee] = useState([]);
+
+
+  const getGuarantee = () => {
+    fetch("garantee.json", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (guaranteeData) {
+        setGuarantee(guaranteeData);
+      });
+  };
+
+    useEffect(() => {
+       getGuarantee()
+    }, ([]))
+
+    console.log(guarantee)
+
+    const Ingredients = productIngredients ? (
+      <div className="product-ingredients">
+        <div
+          className="product-ingredients__title"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <h3>
+            Ingredients
+            <div className="product-ingredients__indicator">
+              {isOpen ? "-" : "+"}
             </div>
-            {isOpen && (
-               productIngredients.map(ingredients => {
-                   return (
-                    <div className="accordion-body">
-                    <li>{ingredients}</li>
-                    </div>
-               )})
-            )}
+          </h3>
         </div>
-   );
+        {isOpen && (
+          <div className="product-ingredients__list">
+            {productIngredients.map((ingredients) => {
+              return <li>{ingredients}</li>;
+            })}
+            <p>
+              Note: Please be aware that ingredient lists may change or vary
+              from time to time. Please refer to the ingredient list on the
+              product package you receive for the most up to date list of
+              ingredients.
+            </p>
+          </div>
+        )}
+      </div>
+    ) : null;
+   
+
+    const Guarantee = productGuarantee
+      ? guarantee.map(function (guarantee) {
+          return (
+            <div className="product-guarantee">
+              <div
+                className="product-guarantee__title"
+                onClick={() => setIsOpen(!isOpen)}
+              >
+                <h3>
+                  {guarantee.gauarantee.title}
+                  <div className="product-guarantee__indicator">
+                    {isOpen ? "-" : "+"}
+                  </div>
+                </h3>
+              </div>
+              {isOpen && <p className="product-guarantee__text">{guarantee.gauarantee.text}</p>}
+            </div>
+          );
+        })
+      : null;
+
+
+    
+
+   
+  
+    return (
+      <div>
+        {Ingredients}
+        {Guarantee}
+      </div>
+    );
  }
