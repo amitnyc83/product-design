b25552e5868bffa472b3ed935c997d07979b6a9c
amitnyc83 <amit_nyc@live.com>
2021-03-05 04:43:33 -0500

    conditional render new text when badge is present

--- src/ProductImage.js
+++ src/ProductImage.js
@@ -1,9 +1,83 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 
-export const ProductImage = ({ image}) => (
-  
+
+
+export const ProductImage = ({ images, badge }) => {
+
+  const [guarantee, setGuarantee] = useState([]);
+  const [selectedImg, setSelectedImg] = useState(images[0])
+
+  const getGuarantee = () => {
+    fetch("garantee.json", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (guaranteeData) {
+        setGuarantee(guaranteeData);
+      });
+  };
+
+    useEffect(() => {
+       getGuarantee()
+    }, ([]))
+
+
+    const mapImages = (images) => {
+      return (
+    images.map((image, idx) => {
+      return (
+        <img 
+         key={idx}
+         src={image}
+         alt="Product"
+         onClick={() => setSelectedImg(image)} 
+        />
+      )
+    }))}
+
+
+
+  return (
   <figure className="product-figure">
-    <img className="product-figure__image" src={image[0]} alt="Product" />
+      <img className="product-figure__image" 
+      src={selectedImg} 
+      alt="Product"
+       />
+         { {badge}.hasOwnProperty("badge") && 
+           guarantee.forEach(guarantee => {
+             for (let i in guarantee) {
+               return (
+                 <div className="product-figure__new-item">{guarantee["new"]["text"]}</div>
+                ) 
+              }
+            })}
+       <div className="product-figure__thumb-bar">
+         {mapImages(images)}
+       </div>
   </figure>
-)
+  )
+}
+
+
+
+
+
+
+// {(() => {
+//   if ({badge}.hasOwnProperty("badge")) {
+//     guarantee.forEach(guarantee => {
+//       for (let i in guarantee) {
+//         console.log(guarantee["new"]["text"])
+//         return (
+//           <div className="product-figure__new-item">{guarantee["new"]["text"]}</div>
+//          ) 
+//        }
+//      }) 
+//    }
+// })()}
